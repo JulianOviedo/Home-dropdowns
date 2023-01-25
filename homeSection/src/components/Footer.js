79eea23526bac4c3d2a3db9deaa40709622618b4
JulianOviedo <julianovie234@gmail.com>
2023-01-25 15:47:25 -0300

    improove footer

--- homeSection/src/components/Footer.js
+++ homeSection/src/components/Footer.js
@@ -5,10 +5,12 @@ const imgs = ['/client-audiophile.svg', '/client-databiz.svg', 'client-maker.svg
 export default function Footer() {
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '20px', height: '36px'}}>
-            {imgs.map((src, index) => {
-                return <img src={src} key={index} alt='brandImg'/>
-            })}
-        </Box>
+        <footer>
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: '20px', alignItems:'center'}}>
+                {imgs.map((src, index) => {
+                    return <img src={src} key={index} alt='brandImg' />
+                })}
+            </Box>
+        </footer>
     )
 }
