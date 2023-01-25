6623a492cd892c677a87d35bdfdb4e391e96143b
JulianOviedo <julianovie234@gmail.com>
2023-01-25 14:42:21 -0300

    add google next font optimization

--- homeSection/src/pages/_app.js
+++ homeSection/src/pages/_app.js
@@ -1,7 +1,12 @@
 import '@/styles/globals.css'
+import { Epilogue } from '@next/font/google'
+
+const epilogue = Epilogue({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <main className={epilogue.className}>
+      <Component {...pageProps} />
+    </main>
   )
 }
