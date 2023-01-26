2cb77f11cdf54b66304b3db57938bf455e546778
JulianOviedo <julianovie234@gmail.com>
2023-01-26 10:52:39 -0300

    add panel img component

---
+++ homeSection/src/components/PanelImg.js
@@ -0,0 +1,28 @@
+import mobileLayout from '../../public/image-hero-mobile.png'
+import desktopLayout from '../../public/image-hero-desktop.png'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+export default function PanelImg() {
+    const [currentImage, setCurrentImage] = useState(desktopLayout);
+
+    const handleResize = () => {
+        if (window.innerWidth < 600) {
+          setCurrentImage(mobileLayout);
+        } else {
+          setCurrentImage(desktopLayout);
+        }
+      }
+
+      useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+      }, []);
+
+
+    return (
+        <figure>
+            <Image src={currentImage} fill alt='panelImg' />
+        </figure>
+    )
+}
