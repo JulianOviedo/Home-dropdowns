1b45c3e8cf6fce3962d077709ced5a36f76deaa4
JulianOviedo <julianovie234@gmail.com>
2023-01-26 13:18:21 -0300

    ref theme

--- homeSection/mui-theme.js
+++ homeSection/mui-theme.js
@@ -13,19 +13,21 @@ export const theme = createTheme({
         }
     },
     typography: {
-        h1: {
-            fontWeight: 700
-        },
-        body1: {
-            fontWeight: 400,
-            color: 'hsl(0, 0%, 41%)'
-        }
     },
     breakpoints: {
         values: {
-          xs: 0,
-          sm: 375, //MOBILE DESIGN
-          md: 1440, //DESKTOP DESIGN
+            xs: 0,
+            sm: 375, //MOBILE DESIGN
+            md: 1440, //DESKTOP DESIGN
         },
-      },
+    },
 });
+
+theme.typography.h1 = {
+    fontWeight: 700,
+}
+
+theme.typography.body1 = {
+    fontWeight: 400,
+    color: 'hsl(0, 0%, 41%)',
+}
