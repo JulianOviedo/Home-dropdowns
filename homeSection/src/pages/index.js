2555b90769f0f6d2847a6491ba5047cdce47b07a
JulianOviedo <julianovie234@gmail.com>
2023-01-26 13:19:01 -0300

    render changes. mobile almost ok

--- homeSection/src/pages/index.js
+++ homeSection/src/pages/index.js
@@ -1,14 +1,22 @@
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import MainSection from "@/components/MainSection/MainSection";
 import PanelImg from "@/components/PanelImg/PanelImg";
+import { Box, useTheme } from "@mui/material";
 
 export default function Home() {
+  const theme = useTheme()
 
   return (
     <>
-      <MainSection/>
-      <PanelImg/>
-      <Footer/>
+      <Header />
+      <Box sx={{display: 'flex', [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column'
+      }}}>
+        <PanelImg />
+        <MainSection />
+      </Box>
+      <Footer />
     </>
   )
 }
