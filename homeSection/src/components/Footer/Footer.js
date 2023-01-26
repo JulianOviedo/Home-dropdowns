7afc4ee1296460f521823334ecca3729a5750a84
JulianOviedo <julianovie234@gmail.com>
2023-01-26 13:18:13 -0300

    ref footer

---
+++ homeSection/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material"
+import styles from './Footer.module.css'
+
+const imgs = ['/client-audiophile.svg', '/client-databiz.svg', 'client-maker.svg', 'client-meet.svg']
+
+export default function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            {imgs.map((src, index) => {
+                return (
+                    <Box sx={{ maxWidth: 114 }}>
+                        <img width='100%' src={src} key={index} alt='brandImg' />
+                    </Box>
+                )
+            })}
+        </footer>
+    )
+}
