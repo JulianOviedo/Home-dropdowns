deeed7449ec9b6be795375a9da6ab640f40b0634
JulianOviedo <julianovie234@gmail.com>
2023-01-26 13:18:34 -0300

    add header component

---
+++ homeSection/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+export default function Header () {
+    return (
+        <header>
+            <Box sx={{px:'10px', py:'20px'}}>
+            <img src="/logo.svg"></img>
+            </Box>
+        </header>
+    )
+}
