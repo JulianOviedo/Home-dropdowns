b4fb2939cafc2f2e0acf876953d8985f2ac2e526
JulianOviedo <julianovie234@gmail.com>
2023-01-27 14:25:46 -0300

    render last commits

--- homeSection/src/pages/index.js
+++ homeSection/src/pages/index.js
@@ -1,19 +1,18 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import MainSection from '@/components/MainSection/MainSection';
-import MyNavbar from '@/components/NavBar/NavBar';
+import NavBar from '@/components/NavBar/NavBar';
 import PanelImg from '@/components/PanelImg/PanelImg';
 import { Box, useTheme } from '@mui/material';
 
 export default function Home() {
   const theme = useTheme()
 
-  const featuresItems = ['Todo List', 'Calendar', 'Reminders', 'Planning']
-  const featuresLogos = ['/icon-todo.svg', '/icon-calendar.svg', null, '/icon-planning.svg']
+
 
   return (
     <>
-    <MyNavbar name='Features' dropdown={true} arrayItems={featuresItems} arrayIcons={featuresLogos}/>
+    <NavBar/>
       {/* <Header />
       <Box sx={{
         display: 'flex', [theme.breakpoints.down('sm')]: {
