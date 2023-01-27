41811bd3376a0c48d48047312bfdcb2cf5414811
JulianOviedo <julianovie234@gmail.com>
2023-01-27 17:10:40 -0300

    add arrow img when dropdowns is open

--- homeSection/src/components/NavItem/NavItem.js
+++ homeSection/src/components/NavItem/NavItem.js
@@ -1,6 +1,10 @@
 import { MenuItem, MenuList, Typography } from '@mui/material'
 import { useState } from 'react'
 
+import arrowDown from '../../../public/icon-arrow-down.svg'
+import arrowUp from '../../../public/icon-arrow-up.svg'
+
+
 
 export default function NavItem({ name, dropdown, arrayItems, arrayIcons }) {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false) //dropdown
@@ -11,10 +15,10 @@ export default function NavItem({ name, dropdown, arrayItems, arrayIcons }) {
     return (
         <>
             {dropdown ?
-                <Typography sx={{p:'5px'}} variant='a' onClick={handleDropdown}>
-                    {name}
+                <Typography sx={{p:'5px', textAlign: 'center'}} variant='a' onClick={handleDropdown}>
+                    {name} {isDropdownOpen ? <img src='icon-arrow-down.svg' alt='arrow'/> : <img alt='arrow' src='icon-arrow-up.svg'/>}
                     {isDropdownOpen &&
-                        <MenuList sx={{borderRadius: '20px', backgroundColor: 'white', boxShadow:' 0 3px 10px rgb(0 0 0 / 0.2)', position:'absolute'}}>
+                        <MenuList sx={{borderRadius: '20px', backgroundColor: 'white', boxShadow:' 0 3px 10px rgb(0 0 0 / 0.2)', position:'absolute',mt:'10px', alignItems:'center'}}>
                             {arrayItems.map((item, idx) => {
                                 return (
                                     <>
