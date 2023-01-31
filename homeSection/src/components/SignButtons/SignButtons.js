2765e6a8cb960fa66f3fe91c714e7320bbc10686
JulianOviedo <julianovie234@gmail.com>
2023-01-31 10:58:26 -0300

    ref buttons

--- homeSection/src/components/SignButtons/SignButtons.js
+++ homeSection/src/components/SignButtons/SignButtons.js
@@ -7,7 +7,7 @@ const SignButtons = () => {
     const theme = useTheme()
     
     return (
-        <Box className={styles['button-container']} sx={{display: 'flex', justifyContent:'flex-end'}}>
+        <Box className={styles['button-container']} sx={{display: {sm: 'flex',xs: 'none'}, justifyContent:'flex-end'}}>
             <Button sx={{width: 100, height: 40, mr: '5px'}} variant='contained'>Login</Button>
             <Button sx={{width:100, height: 40, border:'1px solid hsl(0, 0%, 41%)'}} variant='contained'>Register</Button>
         </Box>
