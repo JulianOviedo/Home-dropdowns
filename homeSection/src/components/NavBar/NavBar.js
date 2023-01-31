aede0bd033aebeed5bd5c17925048268950f6526
JulianOviedo <julianovie234@gmail.com>
2023-01-31 10:58:22 -0300

    ref navbar

--- homeSection/src/components/NavBar/NavBar.js
+++ homeSection/src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
-import { Box } from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 import Image from 'next/image'
+import BurgerMenu from '../BurgerMenu/BurguerMenu'
 import NavItem from '../NavItem/NavItem'
 import SignButtons from '../SignButtons/SignButtons'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+
+    const theme = useTheme()
 
     const featuresItems = ['Todo List', 'Calendar', 'Reminders', 'Planning']
     const featuresLogos = ['/icon-todo.svg', '/icon-calendar.svg', '/icon-reminders.svg', '/icon-planning.svg']
@@ -12,8 +15,9 @@ const NavBar = () => {
     const companyItems = ['History', 'Our Team', 'Blog']
 
     return (
-        <Box component='header' sx={{ display: 'grid', gridTemplateColumns: '10% 70% auto' ,p: '20px', height:40}}>
+        <Box component='header' sx={{ display: 'grid', gridTemplateColumns: '10% 70% auto' ,p: '20px', height:40, [theme.breakpoints.down('sm')]: {display: 'flex', justifyContent: 'space-between'}}}>
             <Image width="84" height="27" src='logo.svg' alt='logo' />
+            <BurgerMenu/>
             <nav className={styles.nav}>
                 <NavItem name='Features' dropdown={true} arrayItems={featuresItems} arrayIcons={featuresLogos} />
                 <NavItem name='Company' dropdown={true} arrayItems={companyItems} />
