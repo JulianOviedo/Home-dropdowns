3c9b5aa4c9a1149dcbf772e4508ff0c09eb78cd9
JulianOviedo <julianovie234@gmail.com>
2023-02-01 13:21:53 -0300

    clean code in sign in buttons

--- homeSection/src/components/SignButtons/SignButtons.js
+++ homeSection/src/components/SignButtons/SignButtons.js
@@ -1,17 +1,16 @@
-import { useTheme } from '@emotion/react'
-import { Box, Button } from '@mui/material'
-import styles from './SignButtons.module.css'
+import { Button } from '@mui/material'
 
 const SignButtons = () => {
 
-    const theme = useTheme()
     
     return (
-        <Box className={styles['button-container']} sx={{display: {sm: 'flex',xs: 'none'}, justifyContent:'flex-end'}}>
+        <>
             <Button sx={{width: 100, height: 40, mr: '5px'}} variant='contained'>Login</Button>
             <Button sx={{width:100, height: 40, border:'1px solid hsl(0, 0%, 41%)'}} variant='contained'>Register</Button>
-        </Box>
+        </>
     )
 }
 
 export default SignButtons
+
+// <Box sx={{display: {sm: 'flex',xs: 'none'}, justifyContent:'flex-end'}}>
